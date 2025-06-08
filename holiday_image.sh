c6f6e144a9fee68be099ebb206b1909d490d9276
Calcium <masteroffoce@gmail.com>
2025-06-08 19:11:11 +0200

    Added pre-generation of next day's background to run.sh

--- holiday_image.sh
+++ holiday_image.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 LANG=en_us_88591
-xdg-open $( python scrape.py $(date +'%B_%d') | python choose_holiday.py | python extract_search_terms.py | python api_get_adress.py  )
+xdg-open $( python scrape.py $(date -d "+$1 day" +'%B_%d') | python choose_holiday.py | python extract_search_terms.py | python api_get_adress.py  )
--- run.sh
+++ run.sh
@@ -3,8 +3,15 @@
 if [ -f "$HOME/backgrounds/$(date +'%Y%m%d')" ]; then
 	echo 'Background for this day already exists.'
 else
-#	./dailybg.sh
-	nix-shell --run './dailybg.sh'
+	echo 'Making background for today.'
+	nix-shell --run './dailybg.sh 0'
 fi
 ./paperconfig.sh
 hyprctl dispatch exec hyprpaper
+
+if [ -f "$HOME/backgrounds/$(date -d "+1 day" +'%Y%m%d')" ]; then
+	echo 'Background for tomorrow already exists.'
+else
+	echo 'Making background for tomorrow.'
+	nix-shell --run './dailybg.sh 1'
+fi
