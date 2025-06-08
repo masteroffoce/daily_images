97cf65439105bad7adac8a3d3a0bd3805a303bd1
Calcium <masteroffoce@gmail.com>
2025-06-08 18:59:50 +0200

    Added an arg to dailybg.sh for which day to find a background for. 0 means today, 1 means tomorrow. Negative numbers work.

--- dailybg.sh
+++ dailybg.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 LANG=en_us_88591
-curl $( python scrape.py $(date +'%B_%d') | python choose_holiday.py | python extract_search_terms.py | python api_get_adress.py ) > ~/backgrounds/$(date +'%Y%m%d')
+curl $( python scrape.py $(date -d "+$1 day" +'%B_%d') | python choose_holiday.py | python extract_search_terms.py | python api_get_adress.py ) > ~/backgrounds/$(date -d "+$1 day" +'%Y%m%d')
