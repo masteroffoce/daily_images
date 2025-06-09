72898e311f28e1bff4383499402377ca2fe24bc6
Calcium <masteroffoce@gmail.com>
2025-06-09 07:41:47 +0200

    Made extract_search_terms.py better by making it motivate its choice. Everything except the first line is removed unless using flag --motivate (or -m).

--- extract_search_terms.py
+++ extract_search_terms.py
@@ -6,7 +6,7 @@ import sys
 holiday = sys.stdin.read().strip()
 data = {
 	"model": "llama3.2:3b",
-	"prompt": f"You're an API that's heping me look for images in a database. I want an image that fits this holiday: {holiday}. Output max two words. The words should lead to a fitting image. Only output the search term. No quotataion marks, nothing more than the word(s). The image should be suitable as a background (no persons, no professions; preferably workplaces). OUTPUT MAX TWO WORDS!!!",
+	"prompt": f"You're an API that's heping me look for images in a database. I want an image that fits this holiday: {holiday}. Output max two words. The words should lead to a fitting image. Only output the search term. No quotataion marks, nothing more than the word(s). The image should be suitable as a background (no persons, no professions; preferably workplaces). OUTPUT MAX TWO WORDS!!! Under the output, make two newlines and motivate your choice.",
 	"stream": False,
 	"options": {
 		"num_predict": -1,
@@ -21,4 +21,11 @@ response = requests.post(url,json=data)
 
 response_json = json.loads(response.text)
 
-print(response_json['response'])
+answer = response_json['response'] 
+
+holiday = answer.partition('\n')[0]
+
+if '-m' in sys.argv or '--motivate' in sys.argv:
+	print(answer)
+else:
+	print(holiday)
